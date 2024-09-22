2f07eb6b87c9e0436cd38b3fd39d0fd2b184e36f
nonafox <admin@jwhgzs.com>
2024-09-22 17:12:28 +0800

    upload

--- project/tmp/20230813/data.php
+++ project/tmp/20230813/data.php
@@ -4,6 +4,9 @@
     list($uid, $json, $file) = app_check('x');
     
     $list = staticcs_list(u('static_user://tmp/20230813'));
+    foreach ($list as $k => $v) {
+        $list[$k]['url'] = u('static://user/tmp/20230813') . '/' . $v['name'];
+    }
     
     api_callback(1, '', ['fileList' => $list]);
 ?>
